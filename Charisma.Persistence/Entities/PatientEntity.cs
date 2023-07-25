71d656fbf076080ecd8b872df2b6608941934b95
Pooya Alamirpour <pooya.alamirpour@gmail.com>
2023-07-26 01:52:43 +0330

    Entities are added

---
+++ Charisma.Persistence/Entities/PatientEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Charisma.Domain.GenericCore.Abstractions;
+using Charisma.Persistence.Common.Constants;
+
+namespace Charisma.Persistence.Entities
+{
+    [Table(TableNames.Appointments)]
+    public class PatientEntity : Entity<Guid>
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Family { get; set; }
+    }
+}
