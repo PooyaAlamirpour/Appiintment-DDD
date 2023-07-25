75bb97981bfe6a7779ac79866f44154e1b25bc50
Pooya Alamirpour <pooya.alamirpour@gmail.com>
2023-07-26 01:52:07 +0330

    Migrations

---
+++ Charisma.Persistence/Migrations/20230705123541_AddingAppointment.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Charisma.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingAppointment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Appointments_Doctors_DoctorEntityId",
+                table: "Appointments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Appointments_DoctorEntityId",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "DoctorEntityId",
+                table: "Appointments");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Speciality",
+                table: "Doctors",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "AppointmentDateTimeEnd",
+                table: "Appointments",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "AppointmentDateTimeStart",
+                table: "Appointments",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "DoctorId",
+                table: "Appointments",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<int>(
+                name: "DurationMinutes",
+                table: "Appointments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "PatientId",
+                table: "Appointments",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateTable(
+                name: "PatientEntity",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Family = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PatientEntity", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_DoctorId",
+                table: "Appointments",
+                column: "DoctorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_PatientId",
+                table: "Appointments",
+                column: "PatientId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Appointments_Doctors_DoctorId",
+                table: "Appointments",
+                column: "DoctorId",
+                principalTable: "Doctors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Appointments_PatientEntity_PatientId",
+                table: "Appointments",
+                column: "PatientId",
+                principalTable: "PatientEntity",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Appointments_Doctors_DoctorId",
+                table: "Appointments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Appointments_PatientEntity_PatientId",
+                table: "Appointments");
+
+            migrationBuilder.DropTable(
+                name: "PatientEntity");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Appointments_DoctorId",
+                table: "Appointments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Appointments_PatientId",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "AppointmentDateTimeEnd",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "AppointmentDateTimeStart",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "DoctorId",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "DurationMinutes",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "PatientId",
+                table: "Appointments");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Speciality",
+                table: "Doctors",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "DoctorEntityId",
+                table: "Appointments",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_DoctorEntityId",
+                table: "Appointments",
+                column: "DoctorEntityId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Appointments_Doctors_DoctorEntityId",
+                table: "Appointments",
+                column: "DoctorEntityId",
+                principalTable: "Doctors",
+                principalColumn: "Id");
+        }
+    }
+}
